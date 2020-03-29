723820f9bdc083be4bbb94c3a1f276f5f7f95ad6
VitaliiPopov <lorenzo.somalia.lxix@gmail.com>
2020-03-29 16:16:41 +0300

    123

--- OpencartTest/src/com/opencart/tools/Driver.java
+++ OpencartTest/src/com/opencart/tools/Driver.java
@@ -25,15 +25,15 @@ public class Driver {
     public static WebDriver getDriver() {
         if (driver == null) {
             if (ConstantVariables.BROWSER_NAME.equalsIgnoreCase("chrome")) {
-                FirefoxOptions options = new FirefoxOptions();
+                ChromeOptions options = new ChromeOptions();
                 options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
                 options.addArguments("--headless");
-                //options.addArguments("--start-maximized");
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver(options);
+                options.addArguments("--start-maximized");
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(options);
             }
         }
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get(ConstantVariables.URL);
         return driver;
